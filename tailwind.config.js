d721134fca0bb06ed0725d94a6dfbc6fbedcf867
maheshmeenabalot <128252442+maheshmeenabalot@users.noreply.github.com>
2024-07-09 16:39:12 +0530

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,16 +2,17 @@
 module.exports = {
   content: ["./src/**/*.{html,js,jsx}"],
   theme: {
-    
     extend: {
-      colors :{
-      'primary' : "#240750",
-      'secondrt' : "#344C64",
-      'light' : "#577B8D",
-
+      colors: {
+        'text': '#240750',     // Dark Purple
+        'secondary': '#344C64',   // Navy Blue
+        'accent': '#577B8D',      // Slate Blue
+        'light': '#1679AB',       // Pale Pink
+        'primary': '#00224D',        // Navy Blue
+        'background': '#FFFFFF',  // White
+        'button-hover': '#E0E0E0', // Gray for button hover
+      },
     },
-  },
   },
   plugins: [],
 }
-
